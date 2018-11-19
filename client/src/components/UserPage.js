89083191e4f4e083082f92e3bbe785c87b6efa9d
Tyana <tyana.stone@gmail.com>
2018-11-18 21:46:05 -0500

    deleted cards

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import styled from 'styled-components'
-
-import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
-    CardSubtitle, CardBody } from 'reactstrap';
-import NavBar from './Navbar';
+import React, { Component } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import add from "../Images/addbutton.png";
+import deleteButton from "../Images/deletebutton.png";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
+import NavBar from "./Navbar";
 
 // Need info about a user
 // Need info about that users ideas
@@ -43,151 +52,142 @@ const NewMovieButton = styled.button`
   color: white;
   font-size: 1.3rem;
   padding: 7.5px 5px;
-`
+`;
 
 const IdeasContainerStyle = styled.div`
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
   align-content: flex-start;
-`
+`;
 
 class UserPage extends Component {
   state = {
     user: {},
     movies: []
-  }
+  };
 
   componentDidMount() {
     // make an api call to get one single user
     // On the server URL is '/api/users/:userId'
-    const userId = this.props.match.params.userId
+    const userId = this.props.match.params.userId;
     axios.get(`/api/users/${userId}`).then(res => {
-      console.log(res.data)
+      console.log(res.data);
       this.setState({
         user: res.data,
         movies: res.data.movies
-      })
-    })
+      });
+    });
   }
 
   handleCreateNewMovie = () => {
-    const userId = this.props.match.params.userId
+    const userId = this.props.match.params.userId;
     const payload = {
-      name: 'Movie Title',
-        image: 'Movie Image',
-      year: 'Movie Year',
-      description: 'Movie Description' 
-    }
+      name: "Movie Title",
+      image: "Movie Image",
+      year: "Movie Year",
+      description: "Movie Description"
+    };
     axios.post(`/api/users/${userId}/movies`, payload).then(res => {
-      const newMovie = res.data
-      const newStateMovies = [...this.state.movies, newMovie]
-      this.setState({ movies: newStateMovies })
-    })
-  }
+      const newMovie = res.data;
+      const newStateMovies = [...this.state.movies, newMovie];
+      this.setState({ movies: newStateMovies });
+    });
+  };
 
   handleDelete = movieId => {
     // some unique value
     axios.delete(`/api/movie/${movieId}`).then(() => {
       //Remove the idea with ideaID from this.state.ideas
-      const newMovies = [...this.state.movies]
+      const newMovies = [...this.state.movies];
       // Return only ideas that are NOT the id provided
       const filtered = newMovies.filter(movie => {
-        return movie._id !== movieId // ! = =
-      })
+        return movie._id !== movieId; // ! = =
+      });
       // Take filtered data and set it to ideas
-      this.setState({movies: filtered})
-    })
-  }
+      this.setState({ movies: filtered });
+    });
+  };
 
   handleChange = (event, movieId) => {
     // const name = event.target.name
     // const value = event.target.value
-    const { value, name } = event.target
-    const newMovies = [...this.state.movies]
+    const { value, name } = event.target;
+    const newMovies = [...this.state.movies];
     const updatedVals = newMovies.map(movie => {
-      if (movie._id === movieId){
-        movie[name] = value
+      if (movie._id === movieId) {
+        movie[name] = value;
       }
-      return movie
-    }) 
+      return movie;
+    });
 
-    this.setState({movies: updatedVals})
+    this.setState({ movies: updatedVals });
+  };
+
+  //   handleUpdate = (userId) => {
+  //     // Find the individual updated idea from this.state.ideas
+  //     const userToUpdate = this.state.users.find(user => {
+  //       return user._id === userId
+  //     })
+  //     // axios post the endpoint with updated data
+  //     axios.patch(`/api/users/${userId}`, userToUpdate).then(() => {
+  //       console.log("Updated User")
+  //     })
+  //     // console .log saved
+  //   }
+  onDelete() {
+    let userId = this.state.user._id;
+    axios
+      .delete(`http://localhost:3000/api/users/${userId}`)
+      .then(response => {
+        this.props.history.push("/");
+      })
+      .catch(err => console.log(err));
   }
 
-//   handleUpdate = (userId) => {
-//     // Find the individual updated idea from this.state.ideas
-//     const userToUpdate = this.state.users.find(user => {
-//       return user._id === userId
-//     })
-//     // axios post the endpoint with updated data
-//     axios.patch(`/api/users/${userId}`, userToUpdate).then(() => {
-//       console.log("Updated User")  
-//     })
-//     // console .log saved
-//   }
-    onDelete() {
-        let userId = this.state.user._id;
-        axios.delete(`http://localhost:3000/api/users/${userId}`)
-            .then(response => {
-            this.props.history.push('/')
-        }).catch(err => console.log(err))
-    }
-
   render() {
-      return (
-        
-          <div>
-              <NavBar />
-              <button onClick={this.onDelete.bind(this)}>Delete User</button>
-           
-              {/* <button onClick={EditUser}>Edit User</button> */}
+    return (
+      <div>
+        <NavBar />
+        <button onClick={this.onDelete.bind(this)}>Delete User</button>
+
+        {/* <button onClick={EditUser}>Edit User</button> */}
         <h1>{this.state.user.username}'s Movie Page</h1>
         <NewMovieButton onClick={this.handleCreateNewMovie}>
           New Movie
         </NewMovieButton>
-             
+
         <IdeasContainerStyle>
           {this.state.movies.map(movie => {
             const deleteMovie = () => {
-              return this.handleDelete(movie._id)
-            }
+              return this.handleDelete(movie._id);
+            };
 
-                      return (
-                        
-                        // <Card>
-                        // <CardImg top width="100%" src={movie.image} alt="Card image cap" />
-                        // <CardBody>
-                        //   <CardTitle>{movie.name}</CardTitle>
-                          
-                        //   <CardText  onBlur={() => this.handleUpdate(movie._id)}
-                        //           onChange={(event) => this.handleChange(event, movie._id)} 
-                        //           name="description" 
-                        //               value={movie.description}>{movie.year}<br />{movie.description}</CardText>
-                        //   <Button onClick={deleteMovie}>Delete</Button>
-                        //   <Button>Add to List</Button>
-                        // </CardBody>
-                        //   </Card>
-                        <Card>
-                        <CardImg src={movie.image} alt="Card image cap" />
-                        <CardBody>
-                          <CardTitle>Card title</CardTitle>
-                          <CardSubtitle>Card subtitle</CardSubtitle>
-                          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                          <Button>Button</Button>
-                        </CardBody>
-                      </Card>
-            
-                      )
-                  })}
+            return (
+              <div>
+                    {movie.name}
+                    <br/>
+                <img src={movie.image} alt="" width="200" />
+                
                   
-              </IdeasContainerStyle>
+
+              <br/>
+                  
+                  <img
+                    src={deleteButton}
+                    onClick={deleteMovie}
+                    alt="addbutton"
+                    width="100"
+                  />
+                  <img src={add} alt="addbutton" width="100" />
               
-   
-    </div>
-      
-    )
+              </div>
+              );
+            })}
+        </IdeasContainerStyle>
+      </div>
+    );
   }
 }
 
-export default UserPage
+export default UserPage;
